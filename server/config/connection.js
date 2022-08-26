908ffe93bc6f4505d82124ba19c37dbec2c1846e
Alana <halo2382001@yahoo.com>
2022-08-26 13:36:01 -0400

    updated connection name to group-project-3

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
+  process.env.MONGODB_URI || "mongodb://localhost/group-project-3",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
