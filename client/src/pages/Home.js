6621d0af1e05408439cc75e93defb44f413ec863
Alana <halo2382001@yahoo.com>
2022-08-31 12:58:12 -0400

    added picture to index.css in client folder

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
+
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN_USER);
