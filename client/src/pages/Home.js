8c40fe01d5b91a416232efd5765051f41288729e
Alana <halo2382001@yahoo.com>
2022-09-01 16:53:28 -0400

    working on no match page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
+import Panda from "../assets/pexels-laura-the-explaura-3608263.jpg";
 
 const loggedIn = Auth.loggedIn();
 
@@ -8,11 +9,18 @@ const Home = () => {
   //modify return statement to say something like "Please Sign Up or Log In to begin searching!", and show the links instead of the input forms
   return (
     <main className="flex-row justify-center mb-4">
+ 
       {loggedIn && <div className="col-12 mb-3"></div>}
-      
+
       <h1>Pawed-Casts</h1>
       <div className="col-12 col-md-6">
         <div className="card">HOME PAGE</div>
+        <img
+        src={Panda}
+        style={{ width: "60%" }}
+        alt="404 sign"
+        className="cat404"
+      ></img>
       </div>
     </main>
   );
