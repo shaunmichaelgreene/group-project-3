236e0ed04f9ea21a64cb7b406321a9811b4640c7
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-09-03 12:21:53 -0400

    tinkering with getting search results to render

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,20 +1,41 @@
 import React from 'react';
 
-const PodcastCard = ({ title, imageURL, numberOfEpisodes, url }) => {
+const PodcastCard = ({ id, title, imageURL, numberOfEpisodes, url }) => {
+  const searchResult = JSON.parse(localStorage.getItem("searchResult"));
+  const resultsArray = searchResult.data.podcasts.data
   
-  return (
-    <div className = "podcast-card">
-      <div className="image-container">
-        <a href={imageURL} alt={title} className="podcast-img"></a>
+  const cardBuilder = resultsArray.map(function(result) {
+    id = result.id;
+    imageURL = result.imageURL;
+    numberOfEpisodes = result.numberOfEpisodes;
+    title = result.title;
+    url = result.url;
+
+    return (
+      <div className="podcast-card">
+        <p>{title}</p>
+        <p>{imageURL}</p>
+        <p>{url}</p>
+        <p>{numberOfEpisodes}</p>
+        <p>{id}</p>
       </div>
-      <div className="project-item-details">
-        <h2 className="podcast-url">
-          <a href={url}>{title}</a>
-        </h2>
-        <p className="Number-of-episodes">{numberOfEpisodes}</p>
-      </div>
-    </div>
-  );
+    )
+  })  
+
+  
+  // return (
+  //   <div className = "podcast-card">
+  //     <div className="image-container">
+  //       <a href={imageURL} alt={title} className="podcast-img"></a>
+  //     </div>
+  //     <div className="project-item-details">
+  //       <h2 className="podcast-url">
+  //         <a href={url}>{title}</a>
+  //       </h2>
+  //       <p className="Number-of-episodes">{numberOfEpisodes}</p>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default PodcastCard;
