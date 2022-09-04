be6348722c16b8731737eb266d53575e8bf38ca2
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-09-04 12:02:07 -0400

    prepping to merge search-results branch with develop

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
-
 import { useQuery } from "@apollo/client";
 import { SEARCH } from "../utils/queries";
 import Auth from "../utils/auth";
@@ -57,7 +56,45 @@ const Search = (props) => {
         }
     };
 
-    // const buttonSearch = async (searchTerm) => {
+    
+    return (
+        <main className="flex-row justify-center mb-4">
+            <div className="col-12 col-md-6">
+                <div className="card">
+                    <h4 className="card-header">
+                        Find your favorite pawed-casts here!
+                    </h4>
+                    <div className="card-body">
+                        <form onSubmit={handleFormSubmit}>
+                            <input
+                                className="form-input"
+                                placeholder="Enter your search term(s) here..."
+                                name="searchInput"
+                                type="searchInput"
+                                id="searchInput"
+                                value={formState.searchInput}
+                                onChange={handleChange}
+                            />
+
+                            <button className="btn d-block w-100" type="submit">
+                                Search!
+                            </button>
+                        </form>
+                    </div>
+                    
+                </div>
+             
+            </div>
+        </main>
+    );
+};
+
+export default Search;
+
+
+//CODE ARCHIVE - DO NOT DELETE
+//button search logic and JSX
+// const buttonSearch = async (searchTerm) => {
     //     console.log(searchTerm);
     //     try {
     //         getPodcastsBySearchTerm(searchTerm) //api call with search term
@@ -103,32 +140,9 @@ const Search = (props) => {
     // let savedPodcasts = JSON.parse(localStorage.getItem("searchResult"));
     // getLocalStorage();
 
-    return (
-        <main className="flex-row justify-center mb-4">
-            <div className="col-12 col-md-6">
-                <div className="card">
-                    <h4 className="card-header">
-                        Find your favorite pawed-casts here!
-                    </h4>
-                    <div className="card-body">
-                        <form onSubmit={handleFormSubmit}>
-                            <input
-                                className="form-input"
-                                placeholder="Enter your search term(s) here..."
-                                name="searchInput"
-                                type="searchInput"
-                                id="searchInput"
-                                value={formState.searchInput}
-                                onChange={handleChange}
-                            />
 
-                            <button className="btn d-block w-100" type="submit">
-                                Search!
-                            </button>
-                        </form>
-                    </div>
-                    <h4>Or choose from one of our popular categories!</h4>
-                    {/* <div className="search-btn-container">
+    {/*<h4>Or choose from one of our popular categories!</h4>
+                    <div className="search-btn-container">
             <button onClick={(e) => buttonSearch(e.target.textContent)}>
               Dogs
             </button>
@@ -146,18 +160,37 @@ const Search = (props) => {
             </button>
           </div>
         </div> */}
-                </div>
-                <div></div>
 
-                {/* <div className="container"></div>
-                    {savedPodcasts.map(savedPodcast => (
-                    <PodcastCard key={savedPodcast.id} podcast="podcast" />
-                    ))}
-                    </div> */}
-                <div></div>
-            </div>
-        </main>
-    );
-};
+//original search form handler logic: 
+// getPodcastsBySearchTerm(formState.searchInput) //api call with search term
+// .then(function (response) {
+//   if (response.ok) {
+//     //if call is successful...
+//     // console.log(response.json());
+//     response.json().then(function (data) {
+//       //convert response to json
 
-export default Search;
+//       //temporary logic to check if search result exists in local storage or not
+
+//       if (localStorage.getItem("searchResult") === null) {
+//         let searchResult = data; //if localStorage doesn't already contain the search result, add to local storage
+//         localStorage.setItem(
+//           "searchResult",
+//           JSON.stringify(searchResult)
+//         );
+//       } else {
+//         //if localStorage does already contain the data, fetch from local storage
+//         let searchResult = JSON.parse(
+//           localStorage.getItem("searchResult")
+//         );
+//         console.log(searchResult);
+//         //will likely break this up into a separate helper function, just making sure the call logic is working.
+//       }
+//     });
+//   }
+// });
+// console.log(formState.searchInput);
+// } catch (e) {
+// console.error(e);
+// }
+// };
