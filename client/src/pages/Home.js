81a0aeb5104e2ef7744cae200876cff85e059013
Alana <halo2382001@yahoo.com>
2022-09-04 19:03:12 -0400

    playing around with the styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,8 +11,8 @@ const Home = () => {
     <main className="flex-row justify-center mb-4">
       {loggedIn && <div className="col-12 mb-3"></div>}
 
-      <div className="col-12 col-md-6">
-        <div className="card">HOME PAGE
+      <div className="">
+        <div className="homepage-card">HOME PAGE
         <h2>Hello</h2>
         </div>
       </div>
