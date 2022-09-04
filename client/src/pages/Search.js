9ad59aebc69b886d694b9dccc0c4a296cf596c2a
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-09-04 00:53:20 -0400

    working on statement to redirect to search-results after query response

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 import { useQuery } from "@apollo/client";
 import { SEARCH } from "../utils/queries";
@@ -8,127 +9,125 @@ import { getPodcastsBySearchTerm } from "../utils/API";
 import PodcastCard from "../components/Cards/Cards";
 import { RiSdCardFill } from "react-icons/ri";
 
-
 const Search = (props) => {
-  const [formState, setFormState] = useState({ search: "" });
-  // const [search, { error }] = useQuery(SEARCH);
+    const [formState, setFormState] = useState({ search: "" });
+    // const [search, { error }] = useQuery(SEARCH);
 
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
-
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    //   let searchResult = [];
-
-    if (!searchInput) {
-      return false;
-    }
-
-    try {
-      getPodcastsBySearchTerm(formState.searchInput) //api call with search term
-        .then(function (response) {
-          if (response.ok) {
-            //if call is successful...
-            // console.log(response.json());
-            response.json().then(function (data) {
-              //convert response to json
-
-              //temporary logic to check if search result exists in local storage or not
-
-              if (localStorage.getItem("searchResult") === null) {
-                let searchResult = data; //if localStorage doesn't already contain the search result, add to local storage
-                localStorage.setItem(
-                  "searchResult",
-                  JSON.stringify(searchResult)
-                );
-              } else {
-                //if localStorage does already contain the data, fetch from local storage
-                let searchResult = JSON.parse(
-                  localStorage.getItem("searchResult")
-                );
-                console.log(searchResult);
-                //will likely break this up into a separate helper function, just making sure the call logic is working.
-              }
-            });
-          }
+        setFormState({
+            ...formState,
+            [name]: value,
         });
-      console.log(formState.searchInput);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+    };
 
-  const buttonSearch = async (searchTerm) => {
-    console.log(searchTerm);
-    try {
-      getPodcastsBySearchTerm(searchTerm) //api call with search term
-        .then(function (response) {
-          if (response.ok) {
-            response.json().then(function (data) {
-              if (localStorage.getItem("searchResult") === null) {
-                let searchResult = data; //if localStorage doesn't already contain the search result, add to local storage
-                localStorage.setItem(
-                  "searchResult",
-                  JSON.stringify(searchResult)
-                );
-              } else {
-                let searchResult = JSON.parse(
-                  localStorage.getItem("searchResult")
-                );
-                console.log(searchResult);
-                console.log(searchResult.data.podcasts.data[0].title);
-                console.log(searchResult.data.podcasts.data[0].imageUrl);
-                console.log(
-                  searchResult.data.podcasts.data[0].numberOfEpisodes
-                );
-                console.log(searchResult.data.podcasts.data[0].url);
-              }
-            });
-          }
-        });
-    } catch (e) {
-      console.error(e);
-    }
-  };
+    // submit form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        //   let searchResult = [];
 
-  const getLocalStorage = () => {
-    localStorage.getItem("searchResult", JSON.stringify("searchResult"));
-  };
-  let savedPodcasts = JSON.parse(localStorage.getItem("searchResult"))
-  getLocalStorage();
+        if (!searchInput) {
+            return false;
+        }
 
-  return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-md-6">
-        <div className="card">
-          <h4 className="card-header">Find your favorite pawed-casts here!</h4>
-          <div className="card-body">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="Enter your search term(s) here..."
-                name="searchInput"
-                type="searchInput"
-                id="searchInput"
-                value={formState.searchInput}
-                onChange={handleChange}
-              />
+        try {
+            getPodcastsBySearchTerm(formState.searchInput) //api call with search term
+                .then(function (response) {
+                    if (response.ok) {
+                        //if call is successful...
+                        response.json().then(function (data) {
+                            //convert response to json
 
-              <button className="btn d-block w-100" type="submit">
-                Search!
-              </button>
-            </form>
-          </div>
-          <h4>Or choose from one of our popular categories!</h4>
-          {/* <div className="search-btn-container">
+                            let searchResult = data; // add to local storage
+                            localStorage.setItem(
+                                "searchResult",
+                                JSON.stringify(searchResult)
+                            );
+                            console.log(searchResult);
+                            <Navigate to="/search-results" />;
+                        });
+                    }
+                });
+            console.log(formState.searchInput);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const buttonSearch = async (searchTerm) => {
+        console.log(searchTerm);
+        try {
+            getPodcastsBySearchTerm(searchTerm) //api call with search term
+                .then(function (response) {
+                    if (response.ok) {
+                        response.json().then(function (data) {
+                            if (localStorage.getItem("searchResult") === null) {
+                                let searchResult = data; //if localStorage doesn't already contain the search result, add to local storage
+                                localStorage.setItem(
+                                    "searchResult",
+                                    JSON.stringify(searchResult)
+                                );
+                            } else {
+                                let searchResult = JSON.parse(
+                                    localStorage.getItem("searchResult")
+                                );
+                                console.log(searchResult);
+                                console.log(
+                                    searchResult.data.podcasts.data[0].title
+                                );
+                                console.log(
+                                    searchResult.data.podcasts.data[0].imageUrl
+                                );
+                                console.log(
+                                    searchResult.data.podcasts.data[0]
+                                        .numberOfEpisodes
+                                );
+                                console.log(
+                                    searchResult.data.podcasts.data[0].url
+                                );
+                            }
+                        });
+                    }
+                });
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const getLocalStorage = () => {
+        localStorage.getItem("searchResult", JSON.stringify("searchResult"));
+    };
+    let savedPodcasts = JSON.parse(localStorage.getItem("searchResult"));
+    getLocalStorage();
+
+    return (
+        <main className="flex-row justify-center mb-4">
+            <div className="col-12 col-md-6">
+                <div className="card">
+                    <h4 className="card-header">
+                        Find your favorite pawed-casts here!
+                    </h4>
+                    <div className="card-body">
+                        <form onSubmit={handleFormSubmit}>
+                            <input
+                                className="form-input"
+                                placeholder="Enter your search term(s) here..."
+                                name="searchInput"
+                                type="searchInput"
+                                id="searchInput"
+                                value={formState.searchInput}
+                                onChange={handleChange}
+                            />
+
+                            <button className="btn d-block w-100" type="submit">
+                                Search!
+                            </button>
+                        </form>
+                    </div>
+                    <h4>Or choose from one of our popular categories!</h4>
+                    {/* <div className="search-btn-container">
             <button onClick={(e) => buttonSearch(e.target.textContent)}>
               Dogs
             </button>
@@ -146,20 +145,17 @@ const Search = (props) => {
             </button>
           </div>
         </div> */}
-        </div>
-        <div>
+                </div>
+                <div></div>
 
-        </div>
-
-               {/* <div className="container"></div>
+                {/* <div className="container"></div>
                     {savedPodcasts.map(savedPodcast => (
                     <PodcastCard key={savedPodcast.id} podcast="podcast" />
                     ))}
                     </div> */}
-            <div>
-        </div> 
-        </div>
-     </main>
+                <div></div>
+            </div>
+        </main>
     );
 };
 
