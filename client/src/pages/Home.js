f39ffe7ff23b32ba426ae6fa0a5c27a0a2350fa6
Alana <halo2382001@yahoo.com>
2022-09-06 11:37:29 -0400

    got api working, changed background picture, working on sizing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
 import { Link } from "react-router-dom";
-import Photo from "../assets/pexels-robert-anthony-carbone-598966.jpg";
+
 
 const Home = () => {
   const loggedIn = Auth.loggedIn();
@@ -14,13 +14,17 @@ const Home = () => {
         <div className="homepage-card">
           <h2>Welcome!</h2>
           {/* below is making sure the search doesn't show unless logged in */}
-          {loggedIn && (
+
+          <div className="container">
+            <h3>Are you on the hunt for some great animal podcasts? Then paws what you're doing and search here! </h3>
+            {loggedIn && (
             <div className="col-12 mb-3">
               <Link to="/search">
                 <h2>Begin Search!</h2>
               </Link>
             </div>
           )}
+          </div>
         </div>
       </div>
     </main>
